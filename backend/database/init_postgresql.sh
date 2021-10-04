5e7fe40f8740e4ccfae81ddd8655d579fde91264
haesun <haesunsw@gmail.com>
2021-10-05 05:24:37 +1100

    create db and table for students

--- backend/database/init_postgresql.sh
+++ backend/database/init_postgresql.sh
@@ -7,9 +7,11 @@ brew services start postgresql
 # Once the service starts, you are good to go ahead and use it. However, as a good practice, you should also stop running the service to free up resources on your local machine. You can use the following command to stop postgres from running.
 # brew services stop postgresql
 
-# Once the postgres server is up and running, the next step is to configure it for use. We are going to create a root user that will have administrator privileges to the database server. Make sure that the service is running and then run the following command.
 
-psql postgres
+# psql postgres
+
+psql ourunsw
+
 
 # You should stop running the service to free up resources on your local machine. 
 # You can use the following command to stop postgres from running.
--- backend/database/ourUNSW.sql
+++ backend/database/ourUNSW.sql
@@ -1,4 +1,29 @@
--- Database for ourUNSW app
+-- Database of ourUNSW app
+-- Table of data about Students
 
--- create student table
-CREATE DATABASE student;
+create table Students (
+	userid      varchar(10),
+	name        varchar(30) NOT NULL,
+    email       varchar(100) NOT NULL,
+    displayName varchar(20) NOT NULL,
+    hashedPwd   varchar(64),
+	degree      varchar(70),
+	primary key (userid)
+);
+
+-- INSERT INTO Students values (
+--     'testu', 
+--     'Lol Clown', 
+--     'lol@gmail.com', 
+--     'random', 
+--     'd1e8a70b5ccab1dc2f56bbf7e99f064a660c08e361a35751b9c483c88943d082',
+--     NULL);
+
+-- INSERT INTO Students values (
+--     'adsp123', 
+--     'Adam Spencer', 
+--     'hi@gmail.com', 
+--     'apple', 
+--     'd1e8a70b5ccab1dc2f56bbf7e99f064a660c08e361a35751b9c483c88943d082',
+--     'Computer Science');
+
