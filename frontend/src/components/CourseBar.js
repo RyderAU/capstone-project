2a82a593a6dbe38066eeb5d05a4e5e7b7a86e17b
ryder <ryder.jacka@gmail.com>
2021-10-05 20:04:15 +1100

    Courses now added to interface from object

--- frontend/src/components/CourseBar.js
+++ frontend/src/components/CourseBar.js
@@ -1,20 +1,40 @@
 import "../App.css";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const CourseBar = () => {
+  useEffect(() => {
+    setStudentCourses(student.courses);
+  }, []);
+
+  // grab student details
+  const student = { courses: ["COMP3900", "COMP4920", "COMP1234"] };
+
+  const [studentCourses, setStudentCourses] = useState([]);
+
+  // setStudentDetails(student.courses);
+
+  console.log(studentCourses);
+
   return (
+    // <nav className="courses-container">
+    //   This student is doing {student.courses}
+    //   <ul className="courses-item">
+    //     <Link to="/subject-one">
+    //       <li>COURSE1</li>
+    //     </Link>
+    //     <Link to="/subject-two">
+    //       <li>COURSE2</li>
+    //     </Link>
+    //     <Link to="/subject-three">
+    //       <li>COURSE3</li>
+    //     </Link>
+    //   </ul>
+    // </nav>
     <nav className="courses-container">
-      <ul className="courses-item">
-        <Link to="/subject-one">
-          <li>COURSE1</li>
-        </Link>
-        <Link to="/subject-two">
-          <li>COURSE2</li>
-        </Link>
-        <Link to="/subject-three">
-          <li>COURSE3</li>
-        </Link>
-      </ul>
+      {studentCourses.map((course) => (
+        <h1>{course}</h1>
+      ))}
     </nav>
   );
 };
