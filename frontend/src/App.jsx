abcf8357152b3bfdeb3be5bcb0b8c14719d00234
ryder <ryder.jacka@gmail.com>
2021-10-07 19:21:07 +1100

    Router in app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 // import pages
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Router>
+        <Switch>
+          <Route exact path="/"></Route>
+          <Route exact path="/dashbaord">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
