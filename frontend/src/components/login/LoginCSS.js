c0b26a53a7dcc5980c0c38063110f09565435af6
Daysure <sandeep.das@student.unsw.edu.au>
2021-10-08 17:31:56 +1100

    auth-front: CSS styling files added for login and sign up page

---
+++ frontend/src/components/login/LoginCSS.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  margin: 8px auto;
+  font-weight: bold;
+  width: 250px;
+  color: #fff
+`;
+
+export const Title = styled.label`
+  // font-family: 'Roboto';
+  font-size: 36px;
+  // color: rgb(26, 17, 145);
+  color: #fff;
+  // margin-top: 120px;
+  // margin-left: 35%;
+  margin: 30px auto;
+  font-weight: bold;
+`;
+
+export const Button = styled.button`
+  background-color: #0d0f99;
+  color: #fff;
+  padding: 14px 20px;
+  margin: 8px auto;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  width: 250px;
+  &:hover {
+    // opacity: 0.8;
+    font-weight: bold;
+  }
+`;
+
+export const ButtonSecondary = styled.button`
+  background-color: #ffffff;
+  border-radius: 10px;
+  color: #0d0f99;
+  padding: 14px 20px;
+  margin: 8px auto;
+  border: 2px solid #0d0f99;
+  cursor: pointer;
+  width: 250px;
+  &:hover {
+    // background-color: #0d0f99;
+    // color: #fff;
+    // font-size: 0.8em;
+    // opacity: 0.8;
+    font-weight: bold;
+  }
+`;
+
+export const Input = styled.input`
+  width: 250px;
+  padding: 12px 20px;
+  margin: 8px auto;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-sizing: border-box;
+`;
+
+export const LoginForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 75px auto;
+  background-color: #0086E8;
+  width: 350px;
+  height: 500px;
+  border-radius: 15px;
+`;
+
+export const SignUpForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 75px auto;
+  background-color: #0086E8;
+  width: 350px;
+  height: 650px;
+  border-radius: 15px;
+`;
