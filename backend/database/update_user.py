ccdb6ad6760e7423f258568ae33555947953fa31
Haesun <58884456+haeSunshim@users.noreply.github.com>
2021-10-09 19:43:17 +1100

    Update update_user.py

--- backend/database/update_user.py
+++ backend/database/update_user.py
@@ -1,10 +1,12 @@
 import psycopg2
 from config import config
-
+import sys
 
 def update_user_data(column1, column2, value1, value2):
-    query = "UPDATE students SET "+column1+"='"+value1+"'"+"WHERE "+column2+"='"+value2+"';"
-
+    column_1 = column1
+    column_2 = column2
+    # value2 = "'; drop database sample; --"
+    query = "UPDATE students SET %s='%s' WHERE %s='%s';" % (column_1, value1, column_2, value2)
     conn = None
     try:
         # read database configuration
@@ -13,21 +15,18 @@ def update_user_data(column1, column2, value1, value2):
         conn = psycopg2.connect(**params)
         # create a new cursor
         cur = conn.cursor()
-
-        cur.execute(query)
+        cur.execute(query, (column1, value1, column2, value2,))
         # commit the changes to the database
         conn.commit()
         # close communication with the database
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        return error
+        
     finally:
         if conn is not None:
             conn.close()
 
 '''usage'''
-update_user_data('display_name', 'student_id', 'newname_pfizer2', 'z5555551')
-
-# UPDATE students
-# SET display_name = 'newname_pfizer2'
-# WHERE student_id = 'z5555551';
+update_user_data('display_name', 'student_id', 'selena', "'; drop database testhack; --")
+# print(update_user_data('display_name', 'student_id', 'selena', "'; drop database testhack; --"))
