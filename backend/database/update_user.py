c3f07986fa473f5cae220cd1a063be1d96b31d65
haesun <haesunsw@gmail.com>
2021-10-20 22:51:30 +1100

    db migration

--- backend/database/update_user.py
+++ backend/database/update_user.py
@@ -1,6 +1,7 @@
 import psycopg2
-from .config import config
+# from .config import config
 import sys
+import urllib.parse as up
 
 def update_user_data(column1, column2, value1, value2):
     column_1 = column1
@@ -9,10 +10,15 @@ def update_user_data(column1, column2, value1, value2):
     query = "UPDATE students SET %s='%s' WHERE %s='%s';" % (column_1, value1, column_2, value2)
     conn = None
     try:
-        # read database configuration
-        params = config()
-        # connect to the PostgreSQL database
-        conn = psycopg2.connect(**params)
+        # # read database configuration
+        # params = config()
+        # # connect to the PostgreSQL database
+        # conn = psycopg2.connect(**params)
+        DATABASE_URL = 'postgres://frnkorza:5n3CB1-5ZcZwHt2y781wKZfhaEFdfjlg@rosie.db.elephantsql.com/frnkorza'
+        url = up.urlparse(DATABASE_URL)
+        conn = psycopg2.connect(database=url.path[1:], 
+            user=url.username, password=url.password, 
+            host=url.hostname, port=url.port)
         # create a new cursor
         cur = conn.cursor()
         cur.execute(query, (column1, value1, column2, value2,))
