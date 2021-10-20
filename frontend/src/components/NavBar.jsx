0e42bb87eef49f157311e5e92900d2727a8faa20
ryder <ryder.jacka@gmail.com>
2021-10-20 19:48:12 +1100

    Progress on timetable component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,55 +1,62 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { StoreContext } from '../Store';
-import { useHistory } from 'react-router-dom'
+import React, { useState } from "react";
+import axios from "axios";
+import { StoreContext } from "../Store";
+import { useHistory, NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
   const history = useHistory();
   const context = React.useContext(StoreContext);
-  const [url, ] = context.url;
+  const [url] = context.url;
   const [courses] = context.courses;
   const [token, setToken] = context.token;
 
   // Send request to backend, if request successful move to Landing page
   const handleLogout = () => {
-
     console.log("Logout Button Clicked");
 
-    axios.post(`${url}/logout`, {
-      token: token,
-    })
-      .then(r => {
+    axios
+      .post(`${url}/logout`, {
+        token: token,
+      })
+      .then((r) => {
         handleSuccess(r);
       })
-      .catch(err => {
+      .catch((err) => {
         handleError(err);
       });
-    };
-    
+  };
+
   // Case 1: API returns success
   const handleSuccess = (response) => {
-    console.log('Logout Success');
+    console.log("Logout Success");
     console.log(response);
 
     // Resets token
     setToken("");
     history.push(`/`);
   };
-  
+
   // Case 2: API returns error
   const handleError = (error) => {
-    console.log('Login Failure');
+    console.log("Login Failure");
     console.log(error);
   };
 
-
   return (
-    <nav className="nav-container">
+    <div className="nav-container">
       <ul>
         <button onClick={() => handleLogout()}>Log Out</button>
       </ul>
-    </nav>
+      <button
+        onClick={() => {
+          history.push("/");
+          history.push("dashboard/timetable");
+        }}
+      >
+        timetable
+      </button>
+    </div>
   );
 };
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,22 @@
 import NavBar from "../components/NavBar";
 import CourseBar from "../components/CourseBar";
 import Timetable from "../components/Timetable";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function Dashboard() {
   return (
     <div>
-      <NavBar />
-      <CourseBar />
-      <Timetable />
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path="/dashboard/timetable">
+            <Timetable />
+          </Route>
+          <Route path="/dashboard">
+            <CourseBar />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
