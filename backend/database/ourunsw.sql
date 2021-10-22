b70c0ee826d09c560f5bdb42744c15730b06db93
haesun <haesunsw@gmail.com>
2021-10-22 20:35:24 +1100

    clean sql

--- backend/database/ourunsw.sql
+++ backend/database/ourunsw.sql
@@ -12,25 +12,3 @@ create table Students (
     course          varchar(500),
 	primary key (email)
 );
--- ALTER TABLE Students ALTER COLUMN hashed_pwd VARCHAR(500);
--- INSERT INTO Students values (
---     'z5555555', 
---     'Lol Clown', 
---     'lol@gmail.com', 
---     'random', 
---     'd1e8a70b5ccab1dc2f56bbf7e99f064a660c08e361a35751b9c483c88943d082',
---     NULL,
---     NULL);
-
--- INSERT INTO Students values (
---     'z5555556', 
---     'Adam Spencer', 
---     'hi@gmail.com', 
---     'apple', 
---     'd1e8a70b5ccab1dc2f56bbf7e99f064a660c08e361a35751b9c483c88943d082',
---     '6789dsf',
---     'Computer Science') RETURNING student_id;
-
-
--- DELETE FROM Students
--- WHERE student_id = 'z5555556';
