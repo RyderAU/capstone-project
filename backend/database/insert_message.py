7aad08ebf020508ef0df0a237cddd1040a5a4197
haeSunshim <haesunsw@gmail.com>
2021-10-30 10:50:56 -0400

    fixed bugs and stuff

--- backend/database/insert_message.py
+++ backend/database/insert_message.py
@@ -5,11 +5,11 @@ import psycopg2
 import urllib.parse as up
 
 
-def insert_message(message):
+def insert_message(message_id, message_content, course_id, student_id):
 
     sql = """INSERT INTO messages(
-                message_id, message_content, course_id, student_id)
-                VALUES(%s, %s, %s, %s);"""
+                message_id, message_content, message_time, course_id, student_id)
+                VALUES(%s, %s, current_timestamp, %s, %s);"""
     conn = None
     try:
         DATABASE_URL = 'postgres://frnkorza:5n3CB1-5ZcZwHt2y781wKZfhaEFdfjlg@rosie.db.elephantsql.com/frnkorza'
@@ -31,5 +31,8 @@ def insert_message(message):
         if conn is not None:
             conn.close()
 
+# Driver code
 if __name__ == '__main__':
-    insert_message('4', 'Hello World', 1, 'z5555557')
+    # utilise this code like below:
+    #
+    # insert_message('4', 'Hello World', 1, 'z5555557')
