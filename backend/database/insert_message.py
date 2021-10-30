acbaa5a9892563a0e48ecb6888a1e4f6ccf2154f
haeSunshim <haesunsw@gmail.com>
2021-10-30 04:46:17 -0400

    needs to debug positional argument issue

---
+++ backend/database/insert_message.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+''' Test inserting new message(aka user) into table with python '''
+import psycopg2
+# from .config import config
+import urllib.parse as up
+
+
+def insert_message(message):
+
+    sql = """INSERT INTO messages(
+                message_id, message_content, course_id, student_id)
+                VALUES(%s, %s, %s, %s);"""
+    conn = None
+    try:
+        DATABASE_URL = 'postgres://frnkorza:5n3CB1-5ZcZwHt2y781wKZfhaEFdfjlg@rosie.db.elephantsql.com/frnkorza'
+        url = up.urlparse(DATABASE_URL)
+        conn = psycopg2.connect(database=url.path[1:], 
+            user=url.username, password=url.password, 
+            host=url.hostname, port=url.port)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (message_id, message_content, course_id, student_id))
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+if __name__ == '__main__':
+    insert_message('4', 'Hello World', 1, 'z5555557')
