4c0ca62a96722346ae174efc8922204d1fbc3bae
ryder <ryder.jacka@gmail.com>
2021-11-04 01:38:43 -0400

    Created other profile component

---
+++ frontend/src/components/OtherProfile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OtherProfile = () => {
+    return (
+        <div>
+            hello this is the other profile
+        </div>
+    );
+};
+
+export default OtherProfile;
